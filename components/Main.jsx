47b65ee50a603556884fc87083e770583ccce65e
unknown <hocrux1st.first@gmail.com>
2023-01-16 22:10:19 +0700

    add main and custom to introduce myself

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,10 +3,20 @@ import React from 'react';
 const Main = () => {
     return (
         <div className='w-full h-screen text-center'>
-            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center item-center'>
-                <div> 
-                    
-                    <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHING TOGETHER</p>
+            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+                <div>                 
+                    <p className='uppercase text-sm tracking-widest text-gray-600'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    </p>
+                    <h1 className='py-4 text-gray-700 '>
+                        HI, I'm <span className='text-[#5651e5]'>First</span>
+                    </h1>
+                    <h1 className='py-2 text-gray-700 '>
+                        A Digital Engineering Student
+                    </h1>
+                    <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, rerum laborum cupiditate quibusdam minima enim. Perferendis, ea dolore quidem deleniti voluptatum fugit perspiciatis aliquid eos error voluptatem incidunt ducimus reiciendis.
+                    </p>
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Navbar from '../../components/Navbar'
 import Main from '../../components/Main'
 import './globals.css'
+import Head from './head'
 
 export default function Home() {
   return (
     <div>
+      <Head />
       <Navbar/>
       <Main />
-    </div>
-    
-    
-    
+    </div>   
 
   )
 }
