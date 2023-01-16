c41ba0693ab4078712ff518ab0e86388f0c7d1a6
unknown <hocrux1st.first@gmail.com>
2023-01-16 21:28:30 +0700

    Add event and Main.jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Navbar from '../../components/Navbar'
-
+import Main from '../../components/Main'
 import './globals.css'
 
 export default function Home() {
   return (
     <div>
-      
       <Navbar/>
+      <Main />
     </div>
     
     
