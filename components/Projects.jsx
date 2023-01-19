3a29ad7ab2503216890a27fbc14672d97cd50e7e
unknown <hocrux1st.first@gmail.com>
2023-01-19 11:27:56 +0700

    add smooth scroll global style and add id to components

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,7 +6,7 @@ import netflixImg from '../public/assets/projects/netflix.jpg'
 import twitchImg from '../public/assets/projects/twitch.jpg'
 const Projects = () => {
   return (
-    <div className='w-full'>
+    <div id='projects' className='w-full'>
         <div className='max-w-[1240] mx-auto px-2 y-16'>
             <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
             <h2 className='py-4'>What I've Built</h2>
